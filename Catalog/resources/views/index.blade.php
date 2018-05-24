45640a982ce748a5a876981526db59b4ab9fe70e
bimoan09 <bimoan09@gmail.com>
2018-05-24 22:32:01 +0700

    modified

--- Catalog/resources/views/index.blade.php
+++ Catalog/resources/views/index.blade.php
@@ -6,10 +6,10 @@
         <div class="content">
             <!-- Remove This Before You Start -->
             <h1>Ini adalah catalog sederhana</h1>
-            <p>memkai laravel 5.4</p>
+            <p>memakai laravel 5.4</p>
 
-            <h2>langsung saja klik Tambah Barang</h2>
-          
+            <h2>langsung saja klik Tambah Barang untuk membuat Produk Baru</h2>
+            <h2>langsung saja klik Daftar Barang untuk melihat Produk yang sudah dibuat</h2>
         </div>
         <!-- /.content -->
     </section>
